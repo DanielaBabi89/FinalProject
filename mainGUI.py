9f44f9e92286a111fdac162fede20bfb5bf49642
DanielaBabi89 <46501147+DanielaBabi89@users.noreply.github.com>
2023-01-07 14:57:51 +0200

    done

--- mainGUI.py
+++ mainGUI.py
@@ -8,33 +8,34 @@ from defineGroup import *
 from definePhrase import *
 from tkinter import filedialog
 from LoadTXT import *
-# Const colors:
-PURPLE = "#ADA2FF"
-BLUE = "#C0DEFF"
-PINK = "#FFE5F1"
-YELLOW = "#FFF8E1"
+from tkinter import PhotoImage
+
+# fonts and colors:
+PURPLE = "#7B2869"
+BLUE = "#9D3C72"
+PINK = "#FFBABA"
+YELLOW = "#FFFAE7"
+YELLOW2 = "#E1D7C6"
 RED = "#850000"
 FONT_BODY = ("Verdana", 12, "bold")
 FONT1 = ("Verdana", 30, "bold italic")
 FONT2 = ("Verdana", 10, "bold")
 FONT3 = ("Verdana", 20, "bold")
+FONTB = ("Ariel", 10, "bold")
+FONTB2 = ("JasmineUPC", 9, "bold")
 
 # Create the main window
 window = tk.Tk()
 window.config(bg=YELLOW)
+window.title("Daniela Babi")
 window.state("zoomed")
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
-title = tk.Label(text="SONGS CONCORDANCE", 
-                font=FONT1, 
-                fg=PURPLE, justify=tk.CENTER, 
-                background=YELLOW)
-title.pack()
-title = tk.Label(text="Made By Daniela Babi", 
-                font=FONT1, 
-                fg=BLUE, justify=tk.CENTER, 
-                background=YELLOW)
+# Page Title
+image = PhotoImage(file="logo1.png")
+image = image.subsample(2)
+title = tk.Label(window, image=image, borderwidth=0)
 title.pack()
 
 # Create the first container - MENU
@@ -48,7 +49,6 @@ search_frame.pack(side='left', fill=tk.BOTH)
 # Create the third container - RESULTS
 result_frame = tk.Frame(window, height=screen_height, width=screen_width*(7/10), background=YELLOW)
 result_frame.pack(side='left',fill=tk.BOTH, expand=True)
-
 
 def load_new_song():
     src = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
@@ -313,33 +313,40 @@ def show_songs_search():
     for widget in search_frame.winfo_children():
         widget.destroy()
 
+    # -----> show full song text
     read_song_button = tk.Button(search_frame, text='READ', 
                             command=read_song_on_click)
+    read_song_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     read_song_button.pack()
     
-    # Create the entries and button, and add them to the second container
+    # -----> search by song name
     label = tk.Label(search_frame, text='Song name', background=PINK)
     label.pack()
     song_name_entry = tk.Entry(search_frame)
     song_name_entry.pack()
     search_by_song_button = tk.Button(search_frame, text='search', 
                             command=lambda: search_by_song_button_result(song_name_entry.get()))
+    search_by_song_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_by_song_button.pack()
 
+    # -----> search by artist name
     label = tk.Label(search_frame, text='Artist', background=PINK)
     label.pack()
     artist_entry = tk.Entry(search_frame)
     artist_entry.pack()
     search_by_artist_button = tk.Button(search_frame, text='search',
                             command=lambda: search_by_song_artist_result(artist_entry.get()))
+    search_by_artist_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_by_artist_button.pack()
 
+    # -----> search by word
     label = tk.Label(search_frame, text='Word', background=PINK)
     label.pack()
     word_entry = tk.Entry(search_frame)
     word_entry.pack()
     search_by_word_button = tk.Button(search_frame, text='search',
                                 command=lambda: search_by_song_word_result(word_entry.get()))
+    search_by_word_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_by_word_button.pack()
     
 def show_words_search():
@@ -347,15 +354,17 @@ def show_words_search():
     for widget in search_frame.winfo_children():
         widget.destroy()
 
-    # Create the entries and button, and add them to the second container
+    # -----> search by word
     label = tk.Label(search_frame, text='Word', font=FONT2, background=PINK)
     label.pack()
     word_entry1 = tk.Entry(search_frame)
     word_entry1.pack()
     search_button = tk.Button(search_frame, text='search',
                                 command=lambda: search_word_result(word_entry1.get()))
+    search_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_button.pack()
 
+    # -----> search by length
     label = tk.Label(search_frame, text='Length of Words', font=FONT2, background=PINK)
     label.pack()
     label = tk.Label(search_frame, text='Length', background=PINK)
@@ -364,8 +373,10 @@ def show_words_search():
     length_entry.pack()
     search_by_length_button = tk.Button(search_frame, text='search',
                                     command=lambda: search_word_by_length_result(length_entry.get()))
+    search_by_length_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_by_length_button.pack()  
 
+    # -----> search word in its range
     label = tk.Label(search_frame, text='Range of Word', font=FONT2, background=PINK)
     label.pack()
     label = tk.Label(search_frame, text='Word', background=PINK)
@@ -374,6 +385,7 @@ def show_words_search():
     word_entry6.pack()
     search_range_word_button = tk.Button(search_frame, text='search',
                                     command=lambda: search_range_word_result(word_entry6.get()))
+    search_range_word_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_range_word_button.pack() 
 
 def show_indexes_search():
@@ -381,16 +393,17 @@ def show_indexes_search():
     for widget in search_frame.winfo_children():
         widget.destroy()
 
-    # Query - index by word
+    # -----> search index of word
     label = tk.Label(search_frame, text='Index of word', font=FONT2, background=PINK)
     label.pack()
     word_entry2 = tk.Entry(search_frame)
     word_entry2.pack()
     search_button = tk.Button(search_frame, text='search',
                                 command=lambda: search_index_of_word_result(word_entry2.get()))
+    search_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_button.pack()
 
-    # Query - index of group
+    # -----> search index of group 
     label = tk.Label(search_frame, text='Index of group', font=FONT2, background=PINK)
     label.pack()
     label = tk.Label(search_frame, text='Group', background=PINK)
@@ -399,9 +412,10 @@ def show_indexes_search():
     group_entry2.pack()
     search_button = tk.Button(search_frame, text='search',
                                 command=lambda: search_index_of_group_result(group_entry2.get()))
+    search_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_button.pack()
 
-    # Query - word by index
+    # -----> search by index
     label = tk.Label(search_frame, text='Word in index', font=FONT2, background=PINK)
     label.pack()
     label = tk.Label(search_frame, text='Paragraph', background=PINK)
@@ -414,6 +428,7 @@ def show_indexes_search():
     line_entry.pack()
     search_by_index_button = tk.Button(search_frame, text='search',
                 command=lambda: search_word_by_index_result(paragraph_entry.get(), line_entry.get()))
+    search_by_index_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_by_index_button.pack() 
 
 def show_groups_search():
@@ -421,8 +436,8 @@ def show_groups_search():
     for widget in search_frame.winfo_children():
         widget.destroy()
 
-    # Create the entries and button, and add them to the second container
-    label = tk.Label(search_frame, text='Group Name', font=FONT2)
+    # -----> search by group name
+    label = tk.Label(search_frame, text='Group Name', font=FONT2, background=PINK)
     label.pack()  
     group_name_entry = tk.Entry(search_frame)
     group_name_entry.pack()
@@ -430,40 +445,52 @@ def show_groups_search():
                                     command=lambda: search_group_result(group_name_entry.get()))
     search_index_by_group_button.pack()
 
-    label = tk.Label(search_frame, text='Group by word', font=FONT2)
+    # -----> search group by word
+    label = tk.Label(search_frame, text='Group by word', font=FONT2, background=PINK)
     label.pack()  
-    label = tk.Label(search_frame, text='word')
+    label = tk.Label(search_frame, text='word', background=PINK)
     label.pack()  
     word_entry4 = tk.Entry(search_frame)
     word_entry4.pack()
     search_index_by_group_button = tk.Button(search_frame, text='search',
                                     command=lambda: search_group_by_word_result(word_entry4.get()))
+    search_index_by_group_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_index_by_group_button.pack()
 
-    
+    # > INSERT NEW GROUPS
     label = tk.Label(search_frame, text='*****', background=PINK)
     label.pack(pady=15)
     label = tk.Label(search_frame, text='Select or Type \n words to create\n new group',
                     font=FONT2, background=PINK)
     label.pack()
+    
+    # open word list to choose
     add_group_button = tk.Button(search_frame, text='words list', background=PINK,
                                     command=words_for_group_button)
+    add_group_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     add_group_button.pack() 
+
+    # enter group name
     label = tk.Label(search_frame, text='Group Name', background=PINK)
     label.pack()
     new_group_entry = tk.Entry(search_frame)
     new_group_entry.pack()
 
-    add_group_button = tk.Button(search_frame, text='create for selection',
+    # -----> create group from selection
+    add_group_button = tk.Button(search_frame, text='create from selection',
                                     command=lambda: create_group_from_db(new_group_entry.get()))
+    add_group_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     add_group_button.pack() 
     label = tk.Label(search_frame, text='Words', background=PINK)
     label.pack(pady=5)
     text = tk.Text(search_frame, width=15, height=9)
     text.pack()
 
+    # -----> create group from text
+
     add_group_button1 = tk.Button(search_frame, text='create from text',
                 command=lambda: create_group_from_text(new_group_entry.get(), (text.get('1.0', 'end').split('\n'))))
+    add_group_button1.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     add_group_button1.pack(pady=5)
 
 def show_phrases_search():
@@ -471,40 +498,49 @@ def show_phrases_search():
     for widget in search_frame.winfo_children():
         widget.destroy()
 
-    # Create the entries and button, and add them to the second container
-    label = tk.Label(search_frame, text='Phrase Name')
+    # -----> search phrase by name
+    label = tk.Label(search_frame, text='Phrase Name', background=PINK)
     label.pack()  
     phrase_name_entry = tk.Entry(search_frame)
     phrase_name_entry.pack()
     search_index_by_group_button = tk.Button(search_frame, text='search',
                                     command=lambda: search_phrase_result(phrase_name_entry.get()))
+    search_index_by_group_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_index_by_group_button.pack()
 
-    label = tk.Label(search_frame, text='Phrase by word', font=FONT2)
+    # -----> search phrase by word
+    label = tk.Label(search_frame, text='Phrase by word', font=FONT2, background=PINK)
     label.pack()  
-    label = tk.Label(search_frame, text='word')
+    label = tk.Label(search_frame, text='word', background=PINK)
     label.pack()  
     word_entry5 = tk.Entry(search_frame)
     word_entry5.pack()
     search_index_by_group_button = tk.Button(search_frame, text='search',
                                     command=lambda: search_phrase_by_word_result(word_entry5.get()))
+    search_index_by_group_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_index_by_group_button.pack()
 
-    label = tk.Label(search_frame, text='Phrase in songs', font=FONT2)
+    # -----> search phrase in songs and show context
+    label = tk.Label(search_frame, text='Phrase in songs', font=FONT2, background=PINK)
     label.pack()  
-    label = tk.Label(search_frame, text='phrase')
+    label = tk.Label(search_frame, text='phrase', background=PINK)
     label.pack()  
     word_entry8 = tk.Entry(search_frame)
     word_entry8.pack()
     search_phrase_in_song_button = tk.Button(search_frame, text='search',
                                     command=lambda: search_phrase_in_song_result(word_entry8.get()))
+    search_phrase_in_song_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     search_phrase_in_song_button.pack()
 
+
+    # > INSERT NEW PHRASES
     label = tk.Label(search_frame, text='*****', background=PINK)
     label.pack(pady=15)
     label = tk.Label(search_frame, text='Select or Type \n words to create\n new phrase',
                     font=FONT2, background=PINK)
     label.pack()
+
+    # -----> crate phrase
     label = tk.Label(search_frame, text='Phrase Name', background=PINK)
     label.pack()
     new_phrase_entry = tk.Entry(search_frame)
@@ -517,6 +553,7 @@ def show_phrases_search():
                                     command=lambda: create_phrase_from_text(
                                         new_phrase_entry.get(),
                                         phrase_entry.get()))
+    add_group_button.configure(fg=PURPLE, bg=YELLOW2, font=FONTB2)
     add_group_button.pack() 
 
 def show_statistics_search():
@@ -603,24 +640,28 @@ def statistics_button_default():
 # Create the buttons and add them to the first container
 #lambda: show_songs("Witness")
 songs_button = tk.Button(menu_frame, text='Songs', command=songs_button_default)
-songs_button.pack()
 words_button = tk.Button(menu_frame, text='Words', command=words_button_default)
-words_button.pack()
 indexes_button = tk.Button(menu_frame, text='Indexes', command=indexes_button_default)
-indexes_button.pack()
 groups_button = tk.Button(menu_frame, text='Groups', command=groups_button_default)
-groups_button.pack()
 phrases_button = tk.Button(menu_frame, text='Phrases', command=phrases_button_default)
+phrases_button = tk.Button(menu_frame, text='Statistics', command=statistics_button_default)
+add_button = tk.Button(menu_frame, text='Add New Song', command=load_new_song)
+
+#----------------configure and pack the buttons
+songs_button.configure(width=12, font=FONTB, background=YELLOW, relief="ridge")
+songs_button.pack()
+words_button.configure(width=12, font=FONTB, background=YELLOW)
+words_button.pack()
+indexes_button.configure(width=12, font=FONTB, background=YELLOW)
+indexes_button.pack()
+groups_button.configure(width=12, font=FONTB, background=YELLOW)
+groups_button.pack()
+phrases_button.configure(width=12, font=FONTB, background=YELLOW)
 phrases_button.pack()
-
-phrases_button = tk.Button(menu_frame, text='Statistics',background=BLUE,
-                             command=statistics_button_default)
+phrases_button.configure(width=12, font=FONTB, background=YELLOW)
 phrases_button.pack()
-
-phrases_button = tk.Button(menu_frame, text='Add New Song', command=load_new_song)
-phrases_button.pack()
-
-
+add_button.configure(width=12, font=FONTB, background=YELLOW)
+add_button.pack()
 
 # Start the main loop
 window.mainloop()
